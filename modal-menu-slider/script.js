19a91efe2149e81871531dd6f92427991583f735
freaky4wrld <freaky4wrld@gmail.com>
2023-09-12 17:29:55 +0530

    created the button functionalities of form and other stuff, provided and overlay

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -1,8 +1,22 @@
 const menuOpenClose = document.getElementById('menu-btn');
 const slidingMenu = document.getElementById('sliding-menu');
+const signupBtn = document.getElementById('sign-up');
+const formShow = document.getElementById('sign-up-form');
+const closeForm = document.getElementById('form-close');
+const overlay = document.getElementById('overlay');
 menuOpenClose.addEventListener('click',()=>{
     menuOpenClose.firstElementChild.classList.toggle('hide');
     menuOpenClose.lastElementChild.classList.toggle('hide');
     slidingMenu.classList.toggle('hide');
     slidingMenu.classList.toggle('animate');
+    overlay.classList.toggle('hide');
+})
+
+signupBtn.addEventListener('click',()=>{
+    formShow.style.display = 'flex';
+    overlay.classList.remove('hide');
+})
+closeForm.addEventListener('click',()=>{
+    formShow.style.display = 'none';
+    overlay.classList.add('hide');
 })
