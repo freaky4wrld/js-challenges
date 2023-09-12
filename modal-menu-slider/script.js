08c882b1d11592b9ac9ea0d702e1467d506353ef
freaky4wrld <freaky4wrld@gmail.com>
2023-09-12 16:38:20 +0530

    added the sliding animation for navbar styling yet in progress

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -3,4 +3,6 @@ const slidingMenu = document.getElementById('sliding-menu');
 menuOpenClose.addEventListener('click',()=>{
     menuOpenClose.firstElementChild.classList.toggle('hide');
     menuOpenClose.lastElementChild.classList.toggle('hide');
+    slidingMenu.classList.toggle('hide');
+    slidingMenu.classList.toggle('animate');
 })
