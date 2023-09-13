123bc13c833a0bc757306f84ffa57255c1ef1ab9
freaky4wrld <freaky4wrld@gmail.com>
2023-09-13 13:13:27 +0530

    created the money-double  functionality

--- DOM-Array-Methods/script.js
+++ DOM-Array-Methods/script.js
@@ -1,8 +1,9 @@
 const usersArray = [];
 const displayArea = document.getElementById('users-display');
 const addUserBtn = document.getElementById('add-user');
+const moneyDoubleBtn = document.getElementById('double-money');
 function generateUser(){
-    let newObject = new Object();
+    let newObject = {name:'',wealth: 0};
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://randomuser.me/api/', true);
     xhr.onload = function(){
@@ -26,24 +27,58 @@ function addDefaultUser(){
 
 addDefaultUser();
 
-usersArray.forEach((user,index)=>{
-    // console.log(user.name)
-    let row = document.createElement('div');
-    row.className = 'row';
-    let nameCol = document.createElement('div');
-    nameCol.classList.add('col','bold');
-    nameCol.innerText = `${usersArray[0].name}`;
-    let wealthCol = document.createElement('div');
-    wealthCol.classList.add('col','normal');
-    // wealthCol.innerText = `$ ${user.wealth}`;
-    row.appendChild(nameCol);
-    row.appendChild(wealthCol);
-    displayArea.appendChild(row);
-})
+setTimeout(showDefault, 1000)
 
 addUserBtn.addEventListener('click', ()=>{
     generateUser();
+    setTimeout(createNewUser,400);
 })
+
+moneyDoubleBtn.addEventListener('click', moneyDouble)
+
+function showDefault(){
+    usersArray.forEach(user=>{
+        createHTML(user)
+    })
+}
+
+function createHTML(user){
+        let row = document.createElement('div');
+        row.className = 'row';
+        let nameCol = document.createElement('div');
+        nameCol.classList.add('col','bold');
+        nameCol.innerText = user.name;
+        let wealthCol = document.createElement('div');
+        wealthCol.classList.add('col','normal');
+        wealthCol.innerText = `$ ${user.wealth}`;
+        row.appendChild(nameCol);
+        row.appendChild(wealthCol);
+        displayArea.appendChild(row);
+}
+
+function createNewUser(){
+        let row = document.createElement('div');
+        row.className = 'row';
+        let nameCol = document.createElement('div');
+        nameCol.classList.add('col','bold');
+        nameCol.innerText = usersArray[usersArray.length-1].name;
+        let wealthCol = document.createElement('div');
+        wealthCol.classList.add('col','normal');
+        wealthCol.innerText = `$ ${usersArray[usersArray.length-1].wealth}`;
+        row.appendChild(nameCol);
+        row.appendChild(wealthCol);
+        displayArea.appendChild(row);
+}
+
+function moneyDouble(){
+    let doubledArray = usersArray.map((user)=>{
+        return 2*user.wealth;
+    })
+    usersArray.forEach((user,index)=>{
+        user.wealth = doubledArray[index];
+    })
+    document.querySelectorAll('.normal').forEach((wealth,index) => wealth.innerText = `$ ${usersArray[index].wealth}`)
+}
 
 
 
