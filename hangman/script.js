5cf3410a991720243789873cab0c43b63508c3c1
freaky4wrld <freaky4wrld@gmail.com>
2023-09-16 13:05:27 +0530

    created pop-up logic for the UI

--- hangman/script.js
+++ hangman/script.js
@@ -7,18 +7,33 @@ let randomWord = words[Math.floor(Math.random()*words.length)];
 const figureParts = document.querySelectorAll('.fig-part');
 const wordsDisplay = document.querySelector('#right-word');
 const wrongWordsDisplay = document.querySelector('#wrong-words-list');
+const repeatPrompt = document.querySelector('#repeat-prompt');
+const endPrompt = document.querySelector('#game-end-prompt');
+const playAgainBtn = document.querySelector('#play-again');
 let wrongWords = [];
+let correctWords = [];
 let index = 0;
+let endPromptMessage = 'Unfortunately you lost !!😈';
 const allowedCharacters = 'abcdefghijklmnopqrstuvwxyz';
+const finalResultShow = ()=>{
+    if(correctWords.length==randomWord.length){
+        endPromptMessage = 'Congratulations you won!!😇';
+    }
+    if(wrongWords.length==figureParts.length || correctWords.length==randomWord.length){
+        playAgainBtn.previousElementSibling.innerText = endPromptMessage;
+        endPrompt.style.display = 'flex';
+    }
+}
 
 const guess = ()=>{
         window.addEventListener('keypress',(e)=>{
             if(allowedCharacters.search(e.key)!== -1){
                 if(wrongWords.length<figureParts.length){
                     if(randomWord.search(e.key)!== -1){
-                        // console.log(e.key);
-                        rightLetters[randomWord.search(e.key)].classList.add('show');
-        
+                        if(!correctWords.includes(e.key)){
+                            rightLetters[randomWord.search(e.key)].classList.add('show');
+                            correctWords.push(e.key);
+                        }
                     }else {
                         wrongWordsDisplay.parentElement.classList.remove('hide');
                         if(!wrongWords.includes(e.key)){
@@ -28,12 +43,14 @@ const guess = ()=>{
                             figureParts[index].classList.remove('fig-part');
                             index++;
                         }else{
-                            console.log('repeated word');
+                            repeatPrompt.open = true;
+                            setTimeout(()=>{repeatPrompt.open = false;},2000);
                         }
                     }
                 }
             }
         })
+        setInterval(finalResultShow,200);
 }
 
 const displayLetter = (className, parentName,content)=>{
@@ -53,3 +70,5 @@ console.log(randomWord);
 showblanks();
 const rightLetters = document.querySelectorAll('.letters');
 guess();
+
+
